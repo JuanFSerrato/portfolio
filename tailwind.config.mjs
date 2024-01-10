a3a28f89a079c0d0f8171c92a21fb3489f667689
Jufesena16 <jufesena16@gmail.com>
2024-01-09 20:43:53 -0500

    first changes portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      movile: { max: "600px" },
+    },
+  },
+  plugins: [],
+};
