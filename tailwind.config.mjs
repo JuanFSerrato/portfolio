159b2ec27b9533a90b20022295f9dad22dcb13b4
Jufesena16 <jufesena16@gmail.com>
2024-01-12 17:43:52 -0500

    change colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,26 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        beige: {
+          50: "#faf7f2",
+          100: "#f3eee1",
+          200: "#e6dcc2",
+          300: "#d5c49c",
+          400: "#c8ad7e",
+          500: "#b79158",
+          600: "#aa7d4c",
+          700: "#8d6541",
+          800: "#72523a",
+          850: "#543c2a",
+          900: "#5d4431",
+          950: "#130d09",
+        },
+      },
+    },
     screens: {
       movile: { max: "600px" },
     },
